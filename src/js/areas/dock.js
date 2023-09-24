667ee716e3e3777f1a0c343cb7b50b04e54f3225
Hakan Bilgin <hbi99@hotmail.com>
2023-09-24 11:32:47 +0200

    dock events

--- src/js/areas/dock.js
+++ src/js/areas/dock.js
@@ -7,6 +7,8 @@
 		let dock = window.find(`content > div[data-area="dock"]`);
 		this.els = {
 			dock,
+			doc: $(document),
+			playPause: dock.find(`li[data-click="toggle-play"] i`),
 			range: dock.find(".range"),
 		};
 		// bind event handlers
@@ -20,21 +22,32 @@
 		// console.log(event);
 		switch (event.type) {
 			// custom events
-			case "goto-intro":
+			case "goto-intro": break;
 			case "toggle-play":
-			case "toggle-music":
-			case "toggle-sound":
-			case "toggle-fps":
+				value = Self.els.playPause.hasClass("icon-pause")
+					? "icon-play"
+					: "icon-pause";
+				Self.els.playPause.prop({ className: value });
 				break;
+			case "toggle-music": break;
+			case "toggle-sound": break;
+			case "toggle-fps": break;
 		}
 	},
 	doRange(event) {
 		let Self = galcon.dock,
 			Drag = Self.drag;
 		switch (event.type) {
-			case "mousedown": break;
-			case "mousemove": break;
-			case "mouseup": break;
+			case "mousedown":
+				// bind event handlers
+				Self.els.doc.on("mousemove mouseup", Self.doRange);
+				break;
+			case "mousemove":
+				break;
+			case "mouseup":
+				// unbind event handlers
+				Self.els.doc.off("mousemove mouseup", Self.doRange);
+				break;
 		}
 	}
 }
